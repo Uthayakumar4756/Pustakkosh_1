ff01d19dfa9a462aa075934fd0d93546756047ef
uthayakumar <111730347+Uthayakumar4756@users.noreply.github.com>
2022-11-01 18:42:16 +0530

    upload a fibonacci prg

---
+++ fiboRecu.java
@@ -0,0 +1,27 @@
+package simple;
+
+public class fiboRecu {
+
+	static int a = 0;
+	static int b = 1;
+	static int s;
+	//int n = 10;
+
+	static void fiboRecu(int n)
+	{
+		if(n>0){
+		s=a+b;
+		a=b;
+		b=s;
+		System.out.print(" "+s);
+		fiboRecu(n-1);
+		}
+	}
+	public static void main(String[] args) {
+
+		int n=10;
+		System.out.print(a+" "+b);
+		fiboRecu(n);
+	}
+
+}
